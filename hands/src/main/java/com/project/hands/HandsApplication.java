dc2ca8e1e1c54e32eea09cdc1f1bfc148cbd6f8a
Gukss <dongkuk97@naver.com>
2023-06-05 11:12:15 +0900

    feat: 엔티티 매핑 완료

---
+++ hands/src/main/java/com/project/hands/HandsApplication.java
@@ -0,0 +1,27 @@
+package com.project.hands;
+
+import java.util.Locale;
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class HandsApplication {
+
+	@PostConstruct
+	public void started(){
+		//시간 한국으로 변경
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		//위치도 변경
+		Locale.setDefault(Locale.KOREA);
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(HandsApplication.class, args);
+	}
+
+}
---
+++ hands/src/test/java/com/project/hands/HandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
